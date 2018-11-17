3debde03f8876c079543f8e98221c18f814a0e9b
Marcin Chajewski <s14062@pjwstk.edu.pl>
2018-11-17 20:28:01 +0100

    push modelu dla adka

---
+++ WypożyczalniaSamochodówPremium/Areas/Adm/AdmAreaRegistration.cs
@@ -0,0 +1,24 @@
+﻿using System.Web.Mvc;
+
+namespace WypożyczalniaSamochodówPremium.Areas.Adm
+{
+    public class AdmAreaRegistration : AreaRegistration 
+    {
+        public override string AreaName 
+        {
+            get 
+            {
+                return "Adm";
+            }
+        }
+
+        public override void RegisterArea(AreaRegistrationContext context) 
+        {
+            context.MapRoute(
+                "Adm_default",
+                "Adm/{controller}/{action}/{id}",
+                new { action = "Index", id = UrlParameter.Optional }
+            );
+        }
+    }
+}
