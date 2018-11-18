ef2dfb4934b73294a8a7baa7c950b23b9b8ea0a9
Marcin Chajewski <s14062@pjwstk.edu.pl>
2018-11-18 14:47:35 +0100

    Zmiany nazw dbsetów w contextach

---
+++ WypożyczalniaSamochodówPremium/Models/AkcesoriaRepository.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WypożyczalniaSamochodówPremium.Models
+{
+    public class AkcesoriaRepository
+    {
+        WypozyczenieEntities entities = new WypozyczenieEntities();
+
+        public IQueryable<Akcesoria> GetAllAkcesoria()
+        {
+            return entities.Akcesoria;
+        }
+        public Akcesoria
+    }
+}
