7b7ad8b326ce3fd41882b2426aaf049703c9e065
Marcin Chajewski <s14062@pjwstk.edu.pl>
2018-11-18 15:40:01 +0100

    Dodanie Repozytoriów Akcesoria-Opis, zmiany w bazie i edmx (IdAkcesoria => AkcesoriaId, [+]DowodOsobistyId)

--- WypożyczalniaSamochodówPremium/Models/AkcesoriaRepository.cs
+++ WypożyczalniaSamochodówPremium/Models/AkcesoriaRepository.cs
@@ -11,8 +11,23 @@ namespace WypożyczalniaSamochodówPremium.Models
 
         public IQueryable<Akcesoria> GetAllAkcesoria()
         {
-            return entities.Akcesoria;
+            return entities.akcesoria;
         }
-        public Akcesoria
+        public Akcesoria GetAkcesoriaById(int id)
+        {
+            return entities.akcesoria.SingleOrDefault(h => h.IdAkcesoria == id);
+        }
+        public void Add(Akcesoria akcesoria)
+        {
+            entities.akcesoria.Add(akcesoria);
+        }
+        public void Delete(Akcesoria akcesoria)
+        {
+            entities.akcesoria.Remove(akcesoria);
+        }
+        public void Save()
+        {
+            entities.SaveChanges();
+        }
     }
 }
