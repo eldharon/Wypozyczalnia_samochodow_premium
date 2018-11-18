5baadd30398bbb48393bb88cb808150a42b74414
Adskyyy <adrian.bramow@gmail.com>
2018-11-18 13:30:34 +0100

    Validation: - New Calss "CennikValidation.cs" - New Class "OpisValidation.cs"

---
+++ WypożyczalniaSamochodówPremium/Models/CennikValidation.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WypożyczalniaSamochodówPremium.Models
+{
+    [MetadataType(typeof(CennikValidation))]
+    public partial class Cennik
+    {
+    }
+
+    public class CennikValidation
+    {
+        [HiddenInput(DisplayValue = false), ScaffoldColumn(false)]
+        public int CennikId { get; set; }
+        [DisplayName("Opis")]
+        public string Opis { get; set; }
+        [Required(ErrorMessage = "Proszę wprowadzić cenę"), DisplayName("Cena")]
+        public double Cena { get; set; }
+    }
+}
---
+++ WypożyczalniaSamochodówPremium/Models/OpisValidation.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WypożyczalniaSamochodówPremium.Models
+{
+    [MetadataType(typeof(OpisValidation))]
+    public partial class Opis
+    {
+    }
+
+    public class OpisValidation
+    {
+        [HiddenInput(DisplayValue = false), ScaffoldColumn(false)]
+        public int OpisId { get; set; }
+        [Required(ErrorMessage = "Proszę wpisać opis"), DisplayName("Opis ogólny")]
+        public string OpisOgolny { get; set; }
+        [Required(ErrorMessage = "Proszę wpisać opis wyposażenia"), DisplayName("Opis wyposażenia")]
+        public string Wyposazenie { get; set; }
+    }
+}
