ab4789e55636f14a97c9e362c8d6b923365978fe
Adskyyy <adrian.bramow@gmail.com>
2019-01-02 22:03:14 +0100

    Implementacja modulu i widoków (index, szczegóły) dla Osoby i KlientFirma, Zmiana w bazie w encji KlientFirma - niepoprawna nazwa - NazwaFrimy na NazwaFirmy

---
+++ WypożyczalniaSamochodówPremium/Areas/Adm/Controllers/KlientFirmaController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WypożyczalniaSamochodówPremium.Models;
+
+namespace WypożyczalniaSamochodówPremium.Areas.Adm.Controllers
+{
+    public class KlientFirmaController : Controller
+    {
+        KlientFirmaRepository klientfirmaRepository = new KlientFirmaRepository();
+        // GET: Adm/Wydarzenie
+        public ActionResult Index()
+        {
+            var model = klientfirmaRepository.GetAllKlienciFirmy();
+            return View(model);
+        }
+
+        public ActionResult Details(int id)
+        {
+            var wydarzenie = klientfirmaRepository.GetKlientFirmaById(id);
+            return View(wydarzenie);
+        }
+    }
+}
---
+++ WypożyczalniaSamochodówPremium/Areas/Adm/Controllers/OsobaController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WypożyczalniaSamochodówPremium.Models;
+
+namespace WypożyczalniaSamochodówPremium.Areas.Adm.Controllers
+{
+    public class OsobaController : Controller
+    {
+       OsobaRepository osobaRepository = new OsobaRepository();
+        // GET: Adm/Wydarzenie
+        public ActionResult Index()
+        {
+            var model = osobaRepository.FindAllOsoba();
+            return View(model);
+        }
+
+        public ActionResult Details(int id)
+        {
+            var wydarzenie = osobaRepository.GetOsobaById(id);
+            return View(wydarzenie);
+        }
+    }
+}
