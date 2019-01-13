f044b88112a681ae56a24882a4ab24668915d367
Jakub Dorodziński <jakub.dorodzinski@gmail.com>
2019-01-13 15:23:12 +0100

    dodanie Asp Identity

---
+++ WypożyczalniaSamochodówPremium/Models/PracownikSelectList.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WypożyczalniaSamochodówPremium.Models
+{
+    public class PracownikSelectList
+    {
+        static readonly IDictionary<string, string> Stanowisko = new Dictionary<string, string>
+        {
+            {"obsługa","obsługa" },
+            {"kierowca","kierowca" },
+            {"menadżer","menadżer" },
+        };
+
+        public static IDictionary<string, string> StanowikoList
+        {
+            get { return Stanowisko; }
+        }
+    }
+}
