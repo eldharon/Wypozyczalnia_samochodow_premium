aa3fdf71b612376cff452006dca5b4f7b29a26b6
Jakub Dorodziński <jakub.dorodzinski@gmail.com>
2019-01-14 21:06:08 +0100

    częściowa implementacja logowania/rejestracji

--- WypożyczalniaSamochodówPremium/Controllers/AccountController.cs
+++ WypożyczalniaSamochodówPremium/Controllers/AccountController.cs
@@ -151,7 +151,7 @@ namespace WypożyczalniaSamochodówPremium.Controllers
         {
             if (ModelState.IsValid)
             {
-                var user = new ApplicationUser { UserName = model.Email, Email = model.Email };
+                var user = new ApplicationUser { UserName = model.Email, Email = model.Email, UserHash = Guid.NewGuid() };
                 var result = await UserManager.CreateAsync(user, model.Password);
                 if (result.Succeeded)
                 {
---
+++ WypożyczalniaSamochodówPremium/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(WypożyczalniaSamochodówPremium.Startup))]
+namespace WypożyczalniaSamochodówPremium
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
