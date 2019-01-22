3096775af6b6d2b4338464a0ef0326b08bb81aa8
Jakub Dorodziński <jakub.dorodzinski@gmail.com>
2019-01-22 20:40:26 +0100

    poprawki

--- WypożyczalniaSamochodówPremium/Models/WypozyczenieRepository.cs
+++ WypożyczalniaSamochodówPremium/Models/WypozyczenieRepository.cs
@@ -50,38 +50,45 @@ namespace WypożyczalniaSamochodówPremium.Models
         {
 
             var temp = entities.wypozyczenia.Where(x => (x.DataWypozyczenia <= from && x.DataZwrotu >= to)
-       || (x.DataWypozyczenia >= from && x.DataZwrotu <= to)
-       || (x.DataWypozyczenia < from && x.DataZwrotu <= to && x.DataZwrotu > from)
-       || (x.DataWypozyczenia >= from && x.DataZwrotu >= to && x.DataWypozyczenia <= from))
-            .Join(entities.wypozyczeniaSamochody,
-            wyp => wyp.WypozyczenieId,
-            wypsam => wypsam.WypozyczenieId,
-            (wyp, wypsam) => new { Wypozyczenie = wyp, WypSam = wypsam })
-            .Select(x => x.WypSam)
-            .Join(entities.samochody,
-            wypsam => wypsam.SamochodId,
-            sam => sam.SamochodId,
-            (wypsam, sam) => new { WypSam = wypsam, Samochod = sam }).Select(x => x.Samochod);
+           || (x.DataWypozyczenia >= from && x.DataZwrotu <= to)
+           || (x.DataWypozyczenia <= from && x.DataZwrotu <= to && x.DataZwrotu >= from)
+           || (x.DataWypozyczenia >= from && x.DataZwrotu >= to && x.DataWypozyczenia <= to))
+          .Join(entities.wypozyczeniaSamochody,
+                wyp => wyp.WypozyczenieId,
+                wypsam => wypsam.WypozyczenieId,
+                (wyp, wypsam) => new { Wypozyczenie = wyp, WypSam = wypsam })
+                .Select(x => x.WypSam)
+          .Join(entities.samochody,
+                wypsam => wypsam.SamochodId,
+                sam => sam.SamochodId,
+                (wypsam, sam) => new { WypSam = wypsam, Samochod = sam }).Select(x => x.Samochod);
 
-            var wypTemp = (from samochod in temp
+
+            var wypTemp = (from samochod in entities.samochody
                            join wypozyczenieTemp in entities.wypozyczeniaTemp
                            on samochod.SamochodId equals wypozyczenieTemp.SamochodId
                            where wypozyczenieTemp.OsobaId == osobaId
                            select wypozyczenieTemp).Where(x => (x.DataWypozyczenia <= from && x.DataZwrotu >= to)
                            || (x.DataWypozyczenia >= from && x.DataZwrotu <= to)
-                           || (x.DataWypozyczenia < from && x.DataZwrotu <= to && x.DataZwrotu > from)
-                           || (x.DataWypozyczenia >= from && x.DataZwrotu >= to && x.DataWypozyczenia <= from)).Select(x => x.Samochod);
+                           || (x.DataWypozyczenia <= from && x.DataZwrotu <= to && x.DataZwrotu >= from)
+                           || (x.DataWypozyczenia >= from && x.DataZwrotu >= to && x.DataWypozyczenia <= to)).Select(x => x.Samochod);
 
 
+            var list1 = temp.Select(x => x.SamochodId);
+            var list2 = wypTemp.Select(x => x.SamochodId);
 
-            IEnumerable<SelectListItem> models = entities.samochody.Except(wypTemp)
+            var result = entities.samochody.Where(x => !list1.Contains(x.SamochodId) && !list2.Contains(x.SamochodId));
+            var check2 = result.ToList();
+
+
+            IEnumerable<SelectListItem> models = result
                 .OrderBy(x => x.Model)
                 .Where(x => x.Marka == marka)
                 .Select(x =>
                 new SelectListItem
                 {
                     Value = x.SamochodId.ToString(),
-                    Text = (x.Model + ", " +x.RokProdukcji.Year + ", "+x.Kolor),
+                    Text = (x.Model + ", " + x.RokProdukcji.Year + ", " + x.Kolor),
                 }).ToList();
             return new SelectList(models, "Value", "Text");
 
