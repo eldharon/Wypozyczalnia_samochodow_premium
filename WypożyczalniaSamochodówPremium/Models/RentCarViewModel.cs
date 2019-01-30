361651805f50af5dbadf7b04c155a6a8204d4c42
Jakub Dorodziński <jakub.dorodzinski@gmail.com>
2019-01-30 03:58:32 +0100

    wypozyczenia c.d

---
+++ WypożyczalniaSamochodówPremium/Models/RentCarViewModel.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace WypożyczalniaSamochodówPremium.Models
+{
+    public class RentCarViewModel
+    {
+        public IEnumerable<WypozyczenieTemp> CarInCartList { get; set; }
+
+        [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}", ApplyFormatInEditMode = true)]
+        public DateTime DataWypozyczenia { get; set; }
+
+        [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}", ApplyFormatInEditMode = true)]
+        public DateTime DataZwrotu { get; set; }
+        public bool CzyDostarczany { get; set; }
+        public bool CzyKierowca { get; set; }
+        public string AdresDojazdu { get; set; }
+        public int WydarzenieId { get; set; }
+        public Osoba Osoba { get; set; }
+        public IEnumerable<Pracownik> KierowcyList { get; set; }
+
+    }
+}
